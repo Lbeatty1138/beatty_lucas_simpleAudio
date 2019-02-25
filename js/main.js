2c2e091a9d7f253765e9b2bc59692146322ba54f
Luke Beatty <lukebeatty@Lukes-MBP.fc.ca>
2019-02-25 15:46:04 -0500

    adding the ability to change music with a click of the Album Art

--- js/main.js
+++ js/main.js
@@ -3,7 +3,8 @@
 	let aud =		document.querySelector('audio'),
 		play =		document.querySelector('#playButton'),
 		pause =		document.querySelector('#pauseButton'),
-		rewind =	document.querySelector('#rewindButton');
+		rewind =	document.querySelector('#rewindButton'),
+		tracks =	document.querySelectorAll('.trackHolder');
 		// , lets it know that there are more selectors and ; tells it that it's the end
 		// bah Spelling is hard Kat...
 
@@ -26,10 +27,21 @@
 		}
 		//  ^ if the console comes up with Nul on a function i.e. (0) try giving it a value = # ;
 
+		function swapSource() {
+			let currentTrack = this.dataset.curenttrack;
+
+			aud.src = `audio/${currentTrack}`;
+			aud.load();
+			aud.play();
+		}
+
+
 		// add event handling
 		play.addEventListener("click", playAudio);
 		pause.addEventListener("click", pauseAudio);
 		rewind.addEventListener("click", rewindAudio);
+
+		tracks.forEach(track => track.addEventListener("click", swapSource));
 
 
 })();
